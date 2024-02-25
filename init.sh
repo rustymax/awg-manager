e7d32678ac9d858f822dab164881eb283128805e
Bkeenke <128972800+bkeenke@users.noreply.github.com>
2024-02-26 00:15:06 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,12 +9,16 @@ cd /opt/go
 wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+sleep 2
 source /etc/profile
 # if the go version does not show, then update the session
 
 #Install amnezia-go
 git clone https://github.com/amnezia-vpn/amneziawg-go.git /opt/amnezia-go
 cd /opt/amnezia-go
+sleep 1
+source /etc/profile
+sleep 1
 make
 #copy to amneziawg-go
 cp /opt/amnezia-go/amneziawg-go /usr/bin/amneziawg-go
