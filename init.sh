4677a0071a5805c2d0d3f2a4fd451294ed30ccfa
Bkeenke <128972800+bkeenke@users.noreply.github.com>
2024-04-29 10:10:08 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -88,7 +88,7 @@ install_go() {
     mkdir -p /opt/go
     cd /opt/go
     curl -O https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+    rm -rf /usr/local/go && tar -C /usr/local -xvzf go1.22.0.linux-amd64.tar.gz
     echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
     source ~/.profile
     if go version >/dev/null 2>&1; then
