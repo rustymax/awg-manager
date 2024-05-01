c9844d61ab3aa86cf4d18bf5dc9760e13e6c65ed
Bkeenke <128972800+bkeenke@users.noreply.github.com>
2024-05-02 00:03:34 +0300

    Update awg-manager.sh

--- awg-manager.sh
+++ awg-manager.sh
@@ -92,6 +92,7 @@ function add_user_to_server {
     fi
 
     local USER_PUB_KEY=$(cat "keys/${USER}/public.key")
+    local USER_PSK_KEY=$(cat "keys/$USER/psk.key")
     local USER_IP=$(grep -i Address "keys/${USER}/${USER}.conf" | sed 's/Address\s*=\s*//i; s/\/.*//')
 
     if grep "# BEGIN ${USER}$" "$SERVER_NAME.conf" >/dev/null ; then
